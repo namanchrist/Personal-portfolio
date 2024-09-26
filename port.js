7ea0b3a4212b5158cd9e36934a43b96d93aeb0a8
namansethia37 <namansethia5@gmail.com>
2024-09-26 21:30:23 +0530

    portfolio

---
+++ port.js
@@ -0,0 +1,9 @@
+var options = {
+    strings: ["Frontend Developer", "Trekker", "Vlogger", "Web Developer"],
+    typeSpeed: 50,
+    backSpeed: 90,
+    backDelay: 1000,
+    loop: true
+};
+
+var typed = new Typed(".text", options);
